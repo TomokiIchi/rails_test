0466a2d381b69f4bf7d559918da827f94bbb1b23
TomokiIchi <tomoki.ichikawa@gaiax.com>
2021-10-02 20:25:39 +0900

    added admin data field & rais db:migrate

--- db/migrate/20211002101602_create_admins.rb
+++ db/migrate/20211002101602_create_admins.rb
@@ -1,9 +1,15 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
-      t.string :email
+      t.string :name
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.integer :password_reset
 
       t.timestamps
     end
+
+    add_index :admins, :email, unique: true
+
   end
 end
